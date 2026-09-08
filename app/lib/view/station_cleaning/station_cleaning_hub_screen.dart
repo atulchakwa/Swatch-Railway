316ab8e5f7f00f88f7a8b4821672d288f4c2beea
atulchakwa <atulchakwa41@gmail.com>
2026-09-08 13:03:07 +0530

    fix(station-cleaning): show Feedback card on contractor supervisor home screen

--- app/lib/view/station_cleaning/station_cleaning_hub_screen.dart
+++ app/lib/view/station_cleaning/station_cleaning_hub_screen.dart
@@ -129,7 +129,7 @@ class _StationCleaningHubScreenState extends State<StationCleaningHubScreen> {
       case 'CONTRACTOR_ADMIN':
         return {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 35, 36};
       case 'CONTRACTOR_SUPERVISOR':
-        return {0, 1, 9, 30, 32, 33, 34};
+        return {0, 1, 9, 11, 30, 32, 33, 34};
       case 'WORKER':
       case 'RAILWAY_WORKER':
       case 'JANITOR':
